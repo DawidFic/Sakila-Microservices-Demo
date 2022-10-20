9540938c4e6c1224d5cc5ff174a10a570de42361
Dawid Fic <dawidfic67@gmail.com>
2022-10-20 11:54:42 +0100

    Sakilla mySQL database java connection demo

---
+++ src/main/java/com/Sakila_Microservices/SakilaMicroservicesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Sakila_Microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaMicroservicesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaMicroservicesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Sakila_Microservices/SakilaMicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sakila_Microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaMicroservicesDemoApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
